afcad68543d9e460843fb21b1310c2b882c7ddae
arju88nair <arju88nair@gmail.com>
2017-11-16 10:30:00 +0530

    Scrollview

---
+++ App.js
@@ -0,0 +1,80 @@
+import React, { Component } from 'react';
+import { AppRegistry, ScrollView, Image, Text } from 'react-native';
+
+export default class App extends Component {
+  render() {
+      return (
+        <ScrollView>
+          <Text style={{fontSize:96}}>Scroll me plz</Text>
+          <Image style={{width: 50, height: 50}}  source={require('./img/logo-react-native.png')} />
+          <Image style={{width: 50, height: 50}}  source={require('./img/logo-react-native.png')} />
+          <Image style={{width: 50, height: 50}}  source={require('./img/logo-react-native.png')} />
+          <Image style={{width: 50, height: 50}}  source={require('./img/logo-react-native.png')} />
+          <Image style={{width: 50, height: 50}}  source={require('./img/logo-react-native.png')} />
+          <Text style={{fontSize:96}}>If you like</Text>
+          <Image style={{width: 50, height: 50}}  source={require('./img/logo-react-native.png')} />
+          <Image style={{width: 50, height: 50}}  source={require('./img/logo-react-native.png')} />
+          <Image style={{width: 50, height: 50}}  source={require('./img/logo-react-native.png')} />
+          <Image style={{width: 50, height: 50}}  source={require('./img/logo-react-native.png')} />
+          <Image style={{width: 50, height: 50}}  source={require('./img/logo-react-native.png')} />
+          <Text style={{fontSize:96}}>Scrolling down</Text>
+          <Image style={{width: 50, height: 50}}  source={require('./img/logo-react-native.png')} />
+          <Image style={{width: 50, height: 50}}  source={require('./img/logo-react-native.png')} />
+          <Image style={{width: 50, height: 50}}  source={require('./img/logo-react-native.png')} />
+          <Image style={{width: 50, height: 50}}  source={require('./img/logo-react-native.png')} />
+          <Image style={{width: 50, height: 50}}  source={require('./img/logo-react-native.png')} />
+          <Text style={{fontSize:96}}>What's the best</Text>
+          <Image style={{width: 50, height: 50}}  source={require('./img/logo-react-native.png')} />
+          <Image style={{width: 50, height: 50}}  source={require('./img/logo-react-native.png')} />
+          <Image style={{width: 50, height: 50}}  source={require('./img/logo-react-native.png')} />
+          <Image style={{width: 50, height: 50}}  source={require('./img/logo-react-native.png')} />
+          <Image style={{width: 50, height: 50}}  source={require('./img/logo-react-native.png')} />
+          <Text style={{fontSize:96}}>Framework around?</Text>
+          <Image style={{width: 50, height: 50}}  source={require('./img/logo-react-native.png')} />
+          <Image style={{width: 50, height: 50}}  source={require('./img/logo-react-native.png')} />
+          <Image style={{width: 50, height: 50}}  source={require('./img/logo-react-native.png')} />
+          <Image style={{width: 50, height: 50}}  source={require('./img/logo-react-native.png')} />
+          <Image style={{width: 50, height: 50}}  source={require('./img/logo-react-native.png')} />
+          <Text style={{fontSize:80}}>React Native</Text>
+        </ScrollView>
+    );
+  }
+}
+
+
+//export default class App extends Component<{}> {
+//  render() {
+//    return (
+//      <View style={styles.container}>
+//        <Text style={styles.welcome}>
+//          Welcome to React Native!
+//        </Text>
+//        <Text style={styles.instructions}>
+//          To get started, edit App.js
+//        </Text>
+//        <Text style={styles.instructions}>
+//          {instructions}
+//        </Text>
+//      </View>
+//    );
+//  }
+//}
+//
+//const styles = StyleSheet.create({
+//  container: {
+//    flex: 1,
+//    justifyContent: 'center',
+//    alignItems: 'center',
+//    backgroundColor: '#F5FCFF',
+//  },
+//  welcome: {
+//    fontSize: 20,
+//    textAlign: 'center',
+//    margin: 10,
+//  },
+//  instructions: {
+//    textAlign: 'center',
+//    color: '#333333',
+//    marginBottom: 5,
+//  },
+//});
